45f527f87ef04e963b4d7db84cc807329456b004
Aditya-Patel24 <adityasp243@gmail.com>
2024-07-10 17:53:17 +0530

    chore: Update Terms and Conditions text in Details.js

---
+++ task9.1/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import React from 'react';
+import { useEffect, useState } from 'react';
+function App() {
+  return (
+    <div className="App">
+     <h1>Bitcoin Live Data</h1>
+     <BitcoinData/>
+    </div>
+  );
+}
+
+export function BitcoinData() {
+  const [data, setData] = useState([]);
+  const apiGet = async () => {
+    fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
+    .then((response)=> response.json())
+    .then((json) => setData(json.bpi.USD))
+  }
+useEffect(() => {
+  const interval = setInterval(() => {
+    apiGet();
+  }, 1000);
+  return () => clearInterval(interval);
+}
+, []);
+return(
+  <h1>{data.rate}</h1>
+);
+}
+export default App;
